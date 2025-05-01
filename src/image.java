e0fa28b772398c4d9f53d68cda7964c6d0565a92
Ayushi Mishra <umamishra2609@gmail.com>
2025-05-01 22:50:18 +0530

    Initial commit

---
+++ src/image.java
@@ -0,0 +1,19 @@
+
+import javax.swing.ImageIcon;
+
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+
+/**
+ *
+ * @author DELL
+ */
+class image {
+
+    static void setIcon(ImageIcon ii) {
+        throw new UnsupportedOperationException("Not supported yet."); // Generated from nbfs://nbhost/SystemFileSystem/Templates/Classes/Code/GeneratedMethodBody
+    }
+    
+}
